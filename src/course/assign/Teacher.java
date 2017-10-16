fc5f10ccba2beab491bf6c60eac535cf51f3c6df
Ethan <ethanliuzongdong@gmail.com>
2017-10-16 23:36:06 +0800

    Add InitTeacher() functions to Teacher Class

--- src/course/assign/Teacher.java
+++ src/course/assign/Teacher.java
@@ -2,6 +2,7 @@ package course.assign;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import database.connect.SQLConnect;
 
@@ -65,9 +66,62 @@ public class Teacher
 		return status;
 	}
 	
+	/**
+	 * Init a teacher from databases.
+	 * @param teacher_id	teacher id, data type is int.
+	 */
+	public void InitTeacher(int teacher_id)
+	{
+		try 
+		{
+			String sql = "SELECT * FROM teacher WHERE teacher_id = ?";
+			PreparedStatement ps = null;
+	        Connection conn = SQLConnect.connetDB();
+	        ps = conn.prepareStatement(sql);
+	        ps.setInt(1, teacher_id);
+	        ResultSet rs = ps.executeQuery();
+	        while(rs.next())
+	        {
+	        	teacherName = rs.getString("teacher_name");
+	        	teacherWorkaLoad = rs.getDouble("teacher_workload");
+	        }
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e);
+		}
+		finally 
+		{
+			SQLConnect.closeDB();
+		}
+	}
+	
 	public boolean HasITCourse()
 	{
-		return false;
+		boolean status = false;
+		try 
+		{
+			String sql = "SELECT * FROM course_assign, course, teacher WHERE course_assign_id = course";
+			PreparedStatement ps = null;
+	        Connection conn = SQLConnect.connetDB();
+	        ps = conn.prepareStatement(sql);
+	        //ps.setInt(1, course_id);
+	        ResultSet rs = ps.executeQuery();
+	        int counter = rs.getMetaData().getColumnCount();
+	        if (counter >= 1) 
+	        {
+				status = true;
+			}
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e);
+		}
+		finally 
+		{
+			SQLConnect.closeDB();
+		}
+		return status;
 	}
 	
 }
